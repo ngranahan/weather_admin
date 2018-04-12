1e5f90b2d8b6738523d15611de0bd6e987b580e3
bunsushi <tbunsu@gmail.com>
2018-04-12 19:17:28 -0400

    user.js functional

---
+++ user.js
@@ -0,0 +1,17 @@
+var weather = require('weather-js');
+
+var User = function (name, location) {
+    this.name = name;
+    this.location = location;
+    this.date = Date.now();
+    this.getWeather = function () {
+        weather.find({ search: this.location, degreeType: 'F' }, function (err, result) {
+            if (err) console.log(err);
+
+            console.log(JSON.stringify(result, null, 2));
+        });
+    }
+}
+
+// var test = new User('Tegan', 'Philadelphia, PA');
+// test.getWeather();
